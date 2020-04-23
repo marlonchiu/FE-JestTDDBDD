b755d0e3e9989a1be1ed823105867f55150dc456
marlonchiu <Knight1898@outlook.com>
2020-04-24 01:13:00 +0800

    2-7 异步代码的测试方法（2）

--- lesson6/fetchData.js
+++ lesson6/fetchData.js
@@ -13,6 +13,36 @@ import axios from 'axios'
 // }
 
 // 3.
+// export const fetchData = (fn) => {
+//   return axios.get('http://www.dell-lee.com/react/api/demo1.json')
+// }
+
+// 4.
+// export const fetchData = (fn) => {
+//   return axios.get('http://www.dell-lee.com/react/api/demo.json')
+// }
+
+// 5.
+// export const fetchData = (fn) => {
+//   return axios.get('http://www.dell-lee.com/react/api/demo1.json')
+// }
+
+// 6.
+// export const fetchData = (fn) => {
+//   return axios.get('http://www.dell-lee.com/react/api/demo.json')
+// }
+
+// 7.
+// export const fetchData = (fn) => {
+//   return axios.get('http://www.dell-lee.com/react/api/demo.json')
+// }
+
+// 8.
+// export const fetchData = (fn) => {
+//   return axios.get('http://www.dell-lee.com/react/api/demo.json')
+// }
+
+// 9.
 export const fetchData = (fn) => {
   return axios.get('http://www.dell-lee.com/react/api/demo1.json')
 }
