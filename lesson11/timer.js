6dd1d0be79572756a3cbd6533a57797f95ac1743
marlonchiu <Knight1898@outlook.com>
2020-04-25 22:17:49 +0800

    删除不相关代码

--- lesson11/timer.js
+++
@@ -1,9 +0,0 @@
-export default (callback) => {
-  setTimeout(() => {
-    callback()
-    setTimeout(() => {
-      console.log(123)
-      callback()
-    }, 4000)
-  }, 3000)
-}
--- lesson13/demo.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery'
-
-const addDivToBody = () => {
-  $('body').append('<div/>')
-}
-
-export default addDivToBody
